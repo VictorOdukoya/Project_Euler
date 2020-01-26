8c0f61f1db5158ad881a44c8265d85fa150ce9cf
VictorOdukoya <victor.odukoya@outlook.de>
2020-01-26 18:14:59 +0100

    Added the solution

--- Euler_1/Program.cs
+++ Euler_1/Program.cs
@@ -10,6 +10,30 @@ namespace Euler_1
     {
         static void Main(string[] args)
         {
+            //O(n) an O(1)
+            //Console.WriteLine(getSumOfDivisibleBy3_5(1000));
+            Console.WriteLine(sumDivisibleBy(3, 999) + sumDivisibleBy(5, 999) - sumDivisibleBy(15, 999));
+        }
+
+        private static int getSumOfDivisibleBy3_5(int limit)
+        {
+            int result = 0;
+            for (int i = 1; i < limit; i++)
+            {
+                if (i % 3 == 0 || i % 5 == 0)
+                {
+                    result += i;
+                }
+            }
+            return result;
+        }
+        // 3+6+9+12+...+999 = 3*(1+2+3+4...+333)
+        // Summe durch die Gaußsche Summenformel berechnen
+        // (n*(n+1))/2 = 1+2+3+4...+n
+        private static int sumDivisibleBy(int n, int p)
+        {
+            // since we are using integers
+            return n * (p / n) * ((p / n + 1)) / 2;
         }
     }
 }
