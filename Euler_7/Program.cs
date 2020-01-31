f04acc19714b80f8617d78b5383e61926f2f1fe9
VictorOdukoya <victor.odukoya@outlook.de>
2020-01-31 23:20:32 +0100

    Added the solution to Problem 7

---
+++ Euler_7/Program.cs
@@ -0,0 +1,39 @@
+using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace CSharp_Shell
+{
+
+    public static class Program 
+    {
+        public static void Main() 
+        {
+           List<int> primeNumbers = getPrimesToN(10001);
+           
+           Console.WriteLine(primeNumbers[10001-1]);
+        }
+        
+        // computes n amount of primes 
+        public static List<int> getPrimesToN(int n){
+            List<int> primeNumbers = new List<int>();
+            primeNumbers.Add(2);
+            
+        	for(int i = 2; primeNumbers.Count < n; i++){
+        		//check if i is divisble by a prime number
+        		for(int j = 0; j < primeNumbers.Count; j++){
+        			if(i % primeNumbers[j] == 0){
+        				break; // not prime
+        			}
+        			if(j == primeNumbers.Count -1) {
+        				// end reached and no divisor found, so must be a prime
+        				primeNumbers.Add(i)
+        			}
+        		}
+        	}
+        	
+        	return primeNumbers;
+        }
+    }
+}
